30729aa167e7a854f9455d725d061dfc5146151e
AH-dark <ahdark@outlook.com>
2022-05-07 17:42:56 +0800

    Chore: removed unnecessary dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./app";
-import "normalize.css/normalize.css";
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@mui/styles";
 
