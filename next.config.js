3a1f89b3ce8702d2b39a4eacc2c4c7fa783ad8a1
AH-dark <ahdark@outlook.com>
2022-06-23 19:15:15 +0800

    Feat: Docker support based on <https://github.com/vercel/next.js/blob/canary/examples/with-docker>
    see: https://nextjs.org/docs/deployment#docker-image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     swcMinify: true,
     reactStrictMode: true,
     compress: true,
+    experimental: {
+        outputStandalone: process.env.NEXT_MODE === "docker",
+    },
 };
 
 module.exports = nextConfig;
