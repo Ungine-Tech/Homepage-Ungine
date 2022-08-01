a0b1ef57b0826f8dd63ad8da3ff1306051aff17c
AH-dark <ahdark@outlook.com>
2022-08-01 15:13:42 +0800

    Feat: replace the starting year with a year interval in the copyright section.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import classes from "./style.module.scss";
+import dayjs from "dayjs";
 
-const Footer: React.FC = () => (
-    <footer className={classes.root}>
-        <p className={classes.copyright}>
-            {"Copyright © 2021 "}
-            <a href="/" rel="self" color={"inherit"} target={"_self"}>
-                {"Hainan Ungine Times Technology Co.,Ltd."}
-            </a>
-            {" All Right Reserved."}
-        </p>
-        <p className={classes.beian}>
-            <a
-                href="//beian.miit.gov.cn"
-                rel="noreferrer"
-                color={"inherit"}
-                target={"_blank"}
-            >
-                {"琼ICP备2021006880号-2"}
-            </a>
-        </p>
-    </footer>
-);
+const Footer: React.FC = () => {
+    const copyrightYear = ((start: number) => {
+        if (dayjs().year() > start) {
+            return `${start} - ${dayjs().year()}`;
+        }
+
+        return start.toString();
+    })(2021);
+
+    return (
+        <footer className={classes.root}>
+            <p className={classes.copyright}>
+                {`Copyright © ${copyrightYear} `}
+                <a href="/" rel="self" color={"inherit"} target={"_self"}>
+                    {"Hainan Ungine Times Technology Co.,Ltd."}
+                </a>
+                {" All Right Reserved."}
+            </p>
+            <p className={classes.beian}>
+                <a
+                    href="//beian.miit.gov.cn"
+                    rel="noreferrer"
+                    color={"inherit"}
+                    target={"_blank"}
+                >
+                    {"琼ICP备2021006880号-2"}
+                </a>
+            </p>
+        </footer>
+    );
+};
 
 export default Footer;
